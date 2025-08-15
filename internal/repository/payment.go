3cd4e3145526505ea35cc895cc47599fac8b2dc2
Daniel Ursul <ursuldm@gmail.com>
2025-08-15 21:59:29 +0300

    implement payment repo

---
+++ internal/repository/payment.go
@@ -0,0 +1,27 @@
+package repository
+
+import (
+	"context"
+
+	"github.com/jmoiron/sqlx"
+)
+
+type PaymentRepository struct {
+	*sqlx.DB
+}
+
+func NewPaymentRepository(db *sqlx.DB) *PaymentRepository {
+	return &PaymentRepository{db}
+}
+
+func (r *PaymentRepository) UpdateBalance(ctx context.Context, accountID int64, amount float64) error {
+	ctx, cancel := context.WithTimeout(ctx, operationTimeout)
+	defer cancel()
+
+	query := `UPDATE accounts SET balance = balance + $1 WHERE id = $2`
+	if _, err := r.ExecContext(ctx, query, amount, accountID); err != nil {
+		return err
+	}
+
+	return nil
+}
