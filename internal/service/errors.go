ed369a92569b61f2ddc39d0ceb0d0d1c5f53190b
Daniel Ursul <ursuldm@gmail.com>
2025-08-16 04:38:50 +0300

    add transport errors

--- internal/service/errors.go
+++ internal/service/errors.go
@@ -9,4 +9,6 @@ var (
 	ErrCannotSignToken       = errors.New("cannot sign token")
 	ErrCannotParseToken      = errors.New("cannot parse token")
 	ErrAccountNotFound       = errors.New("account not found")
+	ErrTransportNotFound     = errors.New("transport not found")
+	ErrAccessDenied          = errors.New("access denied")
 )
